56f3014626372420994da99aa574e1704f9da7fb
Spedine <tucogamesytthegamesof@gmail.com>
2024-02-13 22:33:10 -0300

    debug: Music Controller

--- src/music_controller.py
+++ src/music_controller.py
@@ -7,14 +7,11 @@ current_playing = False
 
 
 def button_action(button = CTkButton, play_image = CTkImage, pause_image = CTkImage) -> None:
-    print(button)
     global current_playing
     
+    current_playing = not current_playing
     if current_playing:
         button.configure(image=pause_image)
-        print(pause_image)
     else:
         button.configure(image=play_image)
-
-    current_playing = not current_playing
 
