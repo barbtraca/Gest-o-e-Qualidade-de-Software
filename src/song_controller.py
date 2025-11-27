43387abb3a713feb2d71899ca8a52dd13ef6a9da
barbara <barbara.tracanella@gmail.com>
2025-11-26 23:29:11 -0300

    Principais melhorias:
    correção de bug (mixer.mixer_music.unload() > errado)
    
    nomes mais claros
    
    padronização
    
    simplificação
    
    legibilidade

--- src/song_controller.py
+++ src/song_controller.py
@@ -1,92 +1,75 @@
-''' Song Controller '''
+"""Song Controller."""
 import os
 from pygame import mixer
 from customtkinter import CTkImage, CTkButton
 
 mixer.init()
 
-SONG_PATH = "././songs/"
+SONG_PATH = "./songs/"
 DEFAULT_VOLUME = 0.5
-MAX_VOLUME = 1
+MAX_VOLUME = 1.0
 
-class Songs():
-    '''
-        Songs class
-    '''
-    def __init__(self) -> None:
+
+class Songs:
+    """Manage songs, playback and state."""
+
+    def _init_(self) -> None:
         self.current_playing = False
         self.current_song_index = 0
         self.playing = False
-        self.songs = self.find_songs()
+        self.songs = self._find_songs()
         self.current_song_name = ""
 
-    def find_songs(self):
-        '''
-            Find all the song files from the /songs/ folder
-            and append it into the song_files list
-        '''
+    # ----------------------- Song Loading ----------------------- #
+
+    def _find_songs(self):
+        """Return list of song file paths inside /songs/."""
         song_files = []
         for root, _, files in os.walk(SONG_PATH):
             for file in files:
-                # if file.endswith(".mp3"):
-                song_files.append(root + file)
+                song_files.append(os.path.join(root, file))
         return song_files
 
+    # ----------------------- Playback ----------------------- #
+
     def play_song(self):
-        '''
-            Reproduz a música atual. 
-    Se estiver pausada, retoma a reprodução
-        '''
+        """Play or resume current song."""
         if not self.playing:
             current_song = self.songs[self.current_song_index]
             mixer.music.load(current_song)
             mixer.music.set_volume(DEFAULT_VOLUME)
             mixer.music.play()
             self.playing = True
-        else: 
+        else:
             mixer.music.unpause()
 
     def pause_song(self):
-        '''
-           Pausa a música atual.
-        '''
+        """Pause current song."""
         if self.playing:
-         mixer.music.pause()
+            mixer.music.pause()
 
-    def next_song(self): 
-        ''' 
-        Avança para a próxima música da playlist 
-        ''' 
-        self.current_song_index += 1 
+    def next_song(self):
+        """Advance to next song."""
+        self.current_song_index += 1
         if self.playing:
             mixer.music.unload()
 
     def previous_song(self):
-        '''
-            Play the previous song of the track
-        '''
-        try:
-            mixer.mixer_music.unload()
-        except AttributeError:
-            pass
+        """Go to previous song."""
         self.playing = False
         self.current_song_index -= 1
+
         if self.current_song_index < 0:
             print("Reached first song")
             self.current_song_index = 0
 
-    def button_action(self,
-                      button = CTkButton,
-                      play_image = CTkImage,
-                      pause_image = CTkImage) -> None:
-        '''
-            A button trigger to start a song or pause it
-            including the chage of the button img
-        '''
+    # ----------------------- UI Trigger ----------------------- #
 
+    def button_action(self, button: CTkButton, play_image: CTkImage, pause_image: CTkImage):
+        """Handle play/pause button click."""
         self.current_playing = not self.current_playing
-        self.current_song_name = os.path.basename(self.songs[self.
-                                                                 current_song_index]).split(".")[0]
+        self.current_song_name = os.path.basename(self.songs[self.current_song_index]).split(".")[0]
+
         if self.current_playing:
             button.configure(image=pause_image)
             self.play_song()
@@ -94,26 +77,22 @@ class Songs():
             self.pause_song()
             button.configure(image=play_image)
 
+    # ----------------------- Utility ----------------------- #
+
     def set_mixer_volume(self, value):
-        '''
-            Set the song volume as the value from the slider
-        '''
+        """Set volume (slider value is 0–100)."""
         try:
-            mixer.music.set_volume(MAX_VOLUME * (value / 100))
-        except AttributeError:
+            mixer.music.set_volume((value / 100) * MAX_VOLUME)
+        except Exception:
             pass
 
     def stop_player(self):
-        '''
-            Just stop the music to don't have any bugs
-        '''
-        self.current_playing = not self.current_playing
+        """Stop music and reset state."""
+        self.current_playing = False
         mixer.music.stop()
 
-    def reset_variables(self, csi = 0, cp = False, p = False):
-        '''
-            Reset variables
-        '''
+    def reset_variables(self, csi=0, cp=False, p=False):
+        """Reset playback variables."""
         self.current_song_index = csi
         self.current_playing = cp
         self.playing = p
